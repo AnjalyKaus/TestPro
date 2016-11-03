a5e8b8d218269aa887341348fa1e33cdfe7223c9
AnjalyKaus <arn11011@gmail.com>
2016-11-03 11:07:59 +0530

    testingggggg

---
+++ Mvc_Razor/Controllers/MobilestoreController.cs
@@ -0,0 +1,89 @@
+﻿using Mvc_Razor.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Mvc_Razor.Controllers
+{
+    public class MobilestoreController : Controller
+    {
+        //
+        // GET: /Mobilestore/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult InsertMobile() // Calling when we first hit controller.
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult InsertMobile(Mobiledata MB) // Calling on http post (on Submit)
+        {
+            if (ModelState.IsValid) //checking model is valid or not
+            {
+                DBdata objDB = new DBdata(); //calling class DBdata
+                string result = objDB.InsertData(MB); // passing Value to DBClass from model
+                ViewData["result"] = result;
+                ModelState.Clear(); //clearing model
+                return View();
+            }
+            else
+            {
+                ModelState.AddModelError("", "Error in saving data");
+                return View();
+            }
+        }
+        public ActionResult ShowAllMobileDetails(Mobiledata MB)
+        {
+            DBdata objDB = new DBdata(); //calling class DBdata
+            MB.StoreAllData = objDB.SelectAllData();
+            return View(MB);
+        }
+
+        public ActionResult EDITMOBILEDATA(string id)
+        {
+            DBdata objDB = new DBdata(); //calling class DBdata
+            DataSet ds = objDB.SelectAllDatabyID(id);
+            Mobiledata MB = new Mobiledata();
+            MB.MobileID = Convert.ToInt32(ds.Tables[0].Rows[0]["MobileID"].ToString());
+            MB.MobileName = ds.Tables[0].Rows[0]["MobileName"].ToString();
+            MB.MobileIMEno = ds.Tables[0].Rows[0]["MobileIMEno"].ToString();
+            MB.mobileprice = ds.Tables[0].Rows[0]["mobileprice"].ToString();
+            MB.mobileManufacured = ds.Tables[0].Rows[0]["mobileManufacured"].ToString();
+            return View(MB);
+        }
+        [HttpPost]
+        public ActionResult EDITMOBILEDATA(Mobiledata MD)
+        {
+            DBdata objDB = new DBdata(); //calling class DBdata
+            string result = objDB.UpdateData(MD); // passing Value to DBClass from model
+            ViewData["resultUpdate"] = result; // for dislaying message after updating data.
+            return RedirectToAction("ShowAllMobileDetails", "Mobilestore");
+        }
+        public ActionResult DELETEMOBILEDATA(string id)
+        {
+            DBdata objDB = new DBdata(); //calling class DBdata
+            DataSet ds = objDB.SelectAllDatabyID(id);
+            Mobiledata MB = new Mobiledata();
+            MB.MobileID = Convert.ToInt32(ds.Tables[0].Rows[0]["MobileID"].ToString());
+            MB.MobileName = ds.Tables[0].Rows[0]["MobileName"].ToString();
+            MB.MobileIMEno = ds.Tables[0].Rows[0]["MobileIMEno"].ToString();
+            MB.mobileprice = ds.Tables[0].Rows[0]["mobileprice"].ToString();
+            MB.mobileManufacured = ds.Tables[0].Rows[0]["mobileManufacured"].ToString();
+            return View(MB);
+        }
+        [HttpPost]
+        public ActionResult DELETEMOBILEDATA(Mobiledata MD)
+        {
+            DBdata objDB = new DBdata(); //calling class DBdata
+            string result = objDB.DeleteData(MD);
+            return RedirectToAction("ShowAllMobileDetails", "Mobilestore");
+        }
+    }
+}
---
+++ Mvc_Razor/Models/DBdata.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace Mvc_Razor.Models
+{
+    public class DBdata
+    {
+        public string InsertData(Mobiledata MD)
+        {
+            SqlConnection con = null;
+            string result = "";
+            try
+            {
+                con = new SqlConnection("Data Source=SQL5001.Smarterasp.net;Initial Catalog=DB_99C023_youth;User ID=DB_99C023_youth_admin;Password=youth@123");
+                SqlCommand cmd = new SqlCommand("Usp_InsertUpdateDelete", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@MobileID", 0);
+                // i will pass zero to MobileID beacause its Primary .
+                cmd.Parameters.AddWithValue("@MobileName", MD.MobileName);
+                cmd.Parameters.AddWithValue("@MobileIMEno", MD.MobileIMEno);
+                cmd.Parameters.AddWithValue("@mobileprice", MD.mobileprice);
+                cmd.Parameters.AddWithValue("@mobileManufacured", MD.mobileManufacured);
+                cmd.Parameters.AddWithValue("@Query", 1);
+                con.Open();
+                result = cmd.ExecuteScalar().ToString();
+                return result;
+            }
+            catch
+            {
+                return result = "";
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+        public string UpdateData(Mobiledata MD)
+        {
+            SqlConnection con = null;
+            string result = "";
+            try
+            {
+                con = new SqlConnection("Data Source=SQL5001.Smarterasp.net;Initial Catalog=DB_99C023_youth;User ID=DB_99C023_youth_admin;Password=youth@123");
+                SqlCommand cmd = new SqlCommand("Usp_InsertUpdateDelete", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@MobileID", MD.MobileID);
+                cmd.Parameters.AddWithValue("@MobileName", MD.MobileName);
+                cmd.Parameters.AddWithValue("@MobileIMEno", MD.MobileIMEno);
+                cmd.Parameters.AddWithValue("@mobileprice", MD.mobileprice);
+                cmd.Parameters.AddWithValue("@mobileManufacured", MD.mobileManufacured);
+                cmd.Parameters.AddWithValue("@Query", 2);
+                con.Open();
+                result = cmd.ExecuteScalar().ToString();
+                return result;
+            }
+            catch
+            {
+                return result = "";
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+        public string DeleteData(Mobiledata MD)
+        {
+            SqlConnection con = null;
+            string result = "";
+            try
+            {
+                con = new SqlConnection("Data Source=SQL5001.Smarterasp.net;Initial Catalog=DB_99C023_youth;User ID=DB_99C023_youth_admin;Password=youth@123");
+                SqlCommand cmd = new SqlCommand("Usp_InsertUpdateDelete", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@MobileID", MD.MobileID);
+                cmd.Parameters.AddWithValue("@MobileName", null);
+                cmd.Parameters.AddWithValue("@MobileIMEno", null);
+                cmd.Parameters.AddWithValue("@mobileprice", 0);
+                cmd.Parameters.AddWithValue("@mobileManufacured", null);
+                cmd.Parameters.AddWithValue("@Query", 3);
+                con.Open();
+                result = cmd.ExecuteScalar().ToString();
+                return result;
+            }
+            catch
+            {
+                return result = "";
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+        public DataSet SelectAllData()
+        {
+            SqlConnection con = null;
+            string result = "";
+            DataSet ds = null;
+            try
+            {
+                con = new SqlConnection("Data Source=SQL5001.Smarterasp.net;Initial Catalog=DB_99C023_youth;User ID=DB_99C023_youth_admin;Password=youth@123");
+                SqlCommand cmd = new SqlCommand("Usp_InsertUpdateDelete", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@MobileID", 0);
+                cmd.Parameters.AddWithValue("@MobileName", null);
+                cmd.Parameters.AddWithValue("@MobileIMEno", null);
+                cmd.Parameters.AddWithValue("@mobileprice", 0);
+                cmd.Parameters.AddWithValue("@mobileManufacured", null);
+                cmd.Parameters.AddWithValue("@Query", 4);
+                con.Open();
+                SqlDataAdapter da = new SqlDataAdapter();
+                da.SelectCommand = cmd;
+                ds = new DataSet(); da.Fill(ds);
+                return ds;
+            }
+            catch
+            {
+                return ds;
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+        public DataSet SelectAllDatabyID(string MobileID)
+        {
+            SqlConnection con = null;
+            string result = "";
+            DataSet ds = null;
+            try
+            {
+                con = new SqlConnection("Data Source=SQL5001.Smarterasp.net;Initial Catalog=DB_99C023_youth;User ID=DB_99C023_youth_admin;Password=youth@123");
+                SqlCommand cmd = new SqlCommand("Usp_InsertUpdateDelete", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@MobileID", MobileID); // i will pass zero to MobileID beacause its Primary .
+                cmd.Parameters.AddWithValue("@MobileName", null);
+                cmd.Parameters.AddWithValue("@MobileIMEno", null);
+                cmd.Parameters.AddWithValue("@mobileprice", 0);
+                cmd.Parameters.AddWithValue("@mobileManufacured", null);
+                cmd.Parameters.AddWithValue("@Query", 4);
+                con.Open();
+                SqlDataAdapter da = new SqlDataAdapter();
+                da.SelectCommand = cmd;
+                ds = new DataSet();
+                da.Fill(ds);
+                return ds;
+            }
+            catch
+            {
+                return ds;
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+    }
+}
